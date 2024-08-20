0e0cf1ac3944a707172a15500a7993ddc422b809
1211119 <1211119@isep.ipp.pt>
2024-08-20 16:31:09 +0100

    [Implementation] Initial ColorParty Project
    Base project for Color Party.
    -Connection with SQL Server
    -MVC architecture (needs to be changed to Onion)

---
+++ ColorParty/ColorParty/ColorParty/Context/AppDBContext.cs
@@ -0,0 +1,26 @@
+﻿using ColorParty.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ColorParty.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext>contextOptions)
+            :base(contextOptions)
+        {
+            
+        }
+        
+    // Code - Approach
+        
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ ColorParty/ColorParty/ColorParty/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace ColorParty.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+
+    }
+}
